ce2e42d8a186469ba4a728e61bf52b5abad1e31c
ingbunga <20sunrin129@sunrint.hs.kr>
2021-12-23 01:00:10 +0900

    default type, LargeTypedEmitter

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,6 +1,7 @@
 
 
 type EmitterType = [
+    [string, anyArrow]
 ]
 
 type anyArrow = (...args: any[]) => any
@@ -47,3 +48,62 @@ export class TypedEmitter<L extends emitterTypes = EmitterType, KeyUnions = unio
     setMaxListeners(n: number): this;
 }
 
+
+
+type _unwrap<l> =
+    l extends { __rec: never } ? never
+      : l extends {__rec: {__rec: infer R}} ? {__rec: _unwrap<R>} 
+      : l extends {__rec: infer R} ? R 
+      : l
+
+type unwrap<l> =
+    l extends {__rec: infer R}
+      ? unwrap<_unwrap<l>>
+      : l
+  
+type _LfindValue<T extends any[], val extends any> =
+    T["length"] extends 0
+      ? never
+      : val extends T[0][0]
+          ? T[0][1]
+          : {__rec: findValue<Rest<T>, val>}
+  
+type LfindValue<T extends any[], val extends any> =
+    unwrap<_LfindValue<T, val>>
+
+type _LKeys<T extends any[]> = 
+    T["length"] extends 0
+      ? []
+      : {__rec: [T[0][0], ...Keys<Rest<T>>]}
+
+type LKeys<T extends any[]> =
+  unwrap<_LKeys<T>>
+
+type a = [
+  ['a', () => void],
+  [`hello_${string}`, (name: string)=>void]
+]
+
+type b = unionOf<LKeys<a>>
+
+export class LargeTypedEmitter<L extends emitterTypes = EmitterType,
+        //@ts-ignore 
+        KeyUnions = LKeys<L>[number]> {
+    static defaultMaxListeners: number;
+    addListener<U extends KeyUnions>(event: U, listener: LfindValue<L, U>): this;
+    prependListener<U extends KeyUnions>(event: U, listener: LfindValue<L, U>): this;
+    prependOnceListener<U extends KeyUnions>(event: U, listener: LfindValue<L, U>): this;
+    removeListener<U extends KeyUnions>(event: U, listener: LfindValue<L, U>): this;
+    removeAllListeners(event?: KeyUnions): this;
+    once<U extends KeyUnions>(event: U, listener: LfindValue<L, U>): this;
+    on<U extends KeyUnions>(event: U, listener: LfindValue<L, U>): this;
+    off<U extends KeyUnions>(event: U, listener: LfindValue<L, U>): this;
+    // @ts-ignore
+    emit<U extends KeyUnions>(event: U, ...args: Parameters<LfindValue<L, U>>): boolean;
+    eventNames<U extends KeyUnions>(): U[];
+    listenerCount(type: KeyUnions): number;
+    listeners<U extends KeyUnions>(type: U): LfindValue<L, U>[];
+    rawListeners<U extends KeyUnions>(type: U): LfindValue<L, U>[];
+    getMaxListeners(): number;
+    setMaxListeners(n: number): this;
+}
